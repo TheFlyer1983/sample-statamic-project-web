79126225c33c9afb3f346e1de469c0d95bbf4cb5
Paul <paul.livingstone@worldhost.group>
2025-02-16 11:48:47 +0000

    Moving navigation logic to computed property.

--- app/components/NavBar/NavItem.vue
+++ app/components/NavBar/NavItem.vue
@@ -1,20 +1,19 @@
 <script lang="ts" setup>
-defineProps<{
-  navItem: NavigationResource
-}>()
+const props = defineProps<{
+  navItem: NavigationResource;
+}>();
+
+const linkTo = computed(() => {
+  return !props.navItem.page.slug
+    ? props.navItem.page.url
+    : props.navItem.page.slug === 'home'
+      ? '/'
+      : { params: { slug: props.navItem.page.slug } };
+});
 </script>
 
 <template>
-  <NuxtLink
-      :to="
-        !navItem.page.slug
-          ? navItem.page.url
-          : navItem.page.slug === 'home'
-            ? '/'
-            : { params: { slug: navItem.page.slug } }
-      "
-      :external="!navItem.page.slug"
-    >
-      {{ navItem.page.title }}
-    </NuxtLink>
+  <NuxtLink :to="linkTo" :external="!navItem.page.slug">
+    {{ navItem.page.title }}
+  </NuxtLink>
 </template>
